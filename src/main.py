e88d778a1c77fdded5200afc7a6afaae5c182c05
Tanujzope <tanujzope5@gmail.com>
2026-08-21 12:56:01 +0530

    Created run_pipeline() function

--- src/main.py
+++ src/main.py
@@ -1,72 +1,75 @@
 from ingestion_manager import(
-get_incoming_files, validate_fileName, read_configuration,
-match_configuration, validate_date,
-validate_extension, validate_columns,
-get_row_count, move_to_validated, get_validated_files
+    get_incoming_files, validate_fileName, read_configuration,
+    match_configuration, validate_date,
+    validate_extension, validate_columns,
+    get_row_count, move_to_validated, get_validated_files
 )
+
 from preprocessing_engine import (
-preprocessed_file
+    preprocessed_file
 )
+
 from transformation_engine import (
-build_curated_and_semantic
+    build_curated_and_semantic
 )
 
 from retention_manager import archive_file
 
 from audit_logger import(
-archilog_ingestion,
-log_preprocess,
-log_retention
+    archilog_ingestion,
+    log_preprocess,
+    log_retention
 )
 
 
-files = get_incoming_files()
+def run_pipeline():
 
+    files = get_incoming_files()
 
+    # for file in files:
+    #     print(file)
+    #     print(validate_fileName(file))
 
-# for file in files:
-#     print(file)
-#     print(validate_fileName(file))
+    configuration = read_configuration()
+    # print(configuration.keys())
 
-configuration = read_configuration()
-# print(configuration.keys())
+    for file in files:
+        config = match_configuration(file, configuration)
 
-for file in files:
-    config = match_configuration(file, configuration)
+        if config is None:
+            print(f'{file.name} ---> No Matching Configuration')
+            continue
 
-    if config is None:
-        print(f'{file.name} ---> No Matching Configuration')
-        continue
+        validDate = validate_date(file)
+        validExtension = validate_extension(file, config)
 
-    validDate = validate_date(file)
-    validExtension = validate_extension(file, config)
+        if validDate and validExtension:
+            validColumn = validate_columns(file, config)
 
-    if validDate and validExtension:
-        validColumn = validate_columns(file, config)
+            if validColumn:
+                print(f"{file.name} -> VALID")
+                move_to_validated(file)
 
-        if validColumn:
-            print(f"{file.name} -> VALID")
-            validatedFile = move_to_validated(file)
-
-            preprocessed_file(validatedFile)
-
+            else:
+                print(f"{file.name} -> INVALID")
         else:
             print(f"{file.name} -> INVALID")
-    else:
-        print(f"{file.name} -> INVALID")
 
 
-validatedFiles = get_validated_files()
+    validatedFiles = get_validated_files()
 
-for file in validatedFiles:
-    preprocessed_file(file)
-    #print(f"{file.name} ----> Processing Completed")
+    for file in validatedFiles:
+        preprocessed_file(file)
+        #print(f"{file.name} ----> Processing Completed")
 
 
-build_curated_and_semantic()
+    build_curated_and_semantic()
 
-archive_file()
+    archive_file()
 
-archilog_ingestion()
-log_preprocess()
-log_retention()
+    archilog_ingestion()
+    log_preprocess()
+    log_retention()
+
+
+run_pipeline()
